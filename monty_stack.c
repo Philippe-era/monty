40fb16c8c61459dc9cebd4834bfaeb185fb2ffe5
Kgalalelo-Mzi <kgalalelomzingaye@gmail.com>
2023-06-21 05:23:28 -0700

    Hi

---
+++ monty_stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * f_stack – displays the top of the stack
+ * @head: Head stack first in line
+ * @counter: number of lines to be counted you check
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus_file.pattern = 0;
+}
---
+++ monty_sub.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+/**
+  *f_sub- Replacement of stuff
+  *@head: Head stack first in line
+  *@counter: Number of lines to be counted
+  *Return: Always 0 Success 
+ */
+void f_sub(stack_t **head, unsigned int counter)
+{
+	stack_t *help_node;
+	int sus_node, success = 2, node_check;
+
+	help_node = *head;
+	for (node_check = 0; help_node != NULL; node_check++)
+		help_node = help_node->next;
+	if (node_check < success)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fclose(bus_file.file_check);
+		free(bus_file.information);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	help_node = *head;
+	sus_node = help_node->next->n - help_node->n;
+	help_node->next->n = sus_node;
+	*head = help_node->next;
+	free(help_node);
+}
+
